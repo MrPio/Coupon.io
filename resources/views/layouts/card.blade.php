b867a6fd773e6f4bfd999d0adb066e692e0dafe7
SbattellaMattia <tiasba01@gmail.com>
2023-05-11 16:29:23 +0200

    Organizzazione nuove cartelle e inserimento item Card e Login

---
+++ resources/views/layouts/card.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="card.css">
+</head>
+
+<body>
+    <div class="card"></div>
+</body>
+</html>
