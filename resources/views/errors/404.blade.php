db00ee6eec6e9c32a8e27cfd6047a63b22e7f2ac
SbattellaMattia <tiasba01@gmail.com>
2023-05-12 12:05:31 +0200

    Aggiunta footer

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,7 +7,7 @@
     <title>Coupon.io | 404 Not found</title>
 </head>
 
-<body style="background-color: var(--color4)">
+<body style="background-color: var(--quasi_bianco)">
 <div style=" padding: 3rem 2rem;display: grid; row-gap: 3rem; justify-content: center">
     <h1 style="font-family: montserrat_bold,serif;text-align: center;">404</h1>
     <img style="margin: auto" src="{{asset('images/404.gif')}}" alt="">
