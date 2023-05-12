8cba778635fdf673cbc6ed37aa1467d79d807af7
Mihail Bobeica <mihailbobeica.07@gmail.com>
2023-05-12 12:21:11 +0200

    update config database
    rimozione del duplicato della configurazione di sqlite

--- config/database.php
+++ config/database.php
@@ -42,14 +42,6 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
