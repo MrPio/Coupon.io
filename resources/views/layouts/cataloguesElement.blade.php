a27b47b44fdcd613f9d61362b283b40971d1a3c1
NicoloAchilli <s1098682@studenti.univpm.it>
2023-05-12 13:01:36 +0200

    aggiunte viste e relativi css

---
+++ resources/views/layouts/cataloguesElement.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="public/css/partials/cataloguesElement.css">
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div class="container">
+    <img class="img" src="" alt="">
+    <div class="container_description">
+        <h3>Travel</h3>
+        <p>13 Coupons</p>
+    </div>
+    <img class="arrow" src="public/images/arrow.svg" alt="">
+</div>
+</body>
+</html>
---
+++ resources/views/layouts/coupon.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="public/css/partials/coupon.css">
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div class="coupon">
+    <div class="sconto">
+        <h1><b>50%</b></h1>
+        <h3>SCONTO</h3>
+    </div>
+    <img src="" class="image" alt="">
+    <div class="container_description">
+        <p>Festa della Mamma Amazon.it: fino al 50% di sconto su moltissimi prodotti</p>
+    </div>
+
+    <div class="bottom_div">
+        <p style="color: white">Scade il 23/04/2023</p>
+        <button class="button">VEDI L'OFFERTA</button>
+    </div>
+</div>
+</body>
+</html>
