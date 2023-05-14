ea5152c70d19a9f05f14fded263b37f134bf7425
Mihail Bobeica <mihailbobeica.07@gmail.com>
2023-05-14 14:37:21 +0200

    aggiunta la migration company (aziende)

---
+++ database/migrations/2023_05_14_122908_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32)->unique();
+            $table->string('place', 32)->nullable();
+            $table->string('logo', 64)->nullable();
+            $table->string('url', 64)->nullable();
+            $table->string('description', 1024)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Resources\Company;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $companies = [];
+        foreach ($companies as $company){
+            Company::create($company);
+        }
+    }
+}
