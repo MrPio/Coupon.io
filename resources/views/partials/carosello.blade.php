57765511a3ccdbc48e2dbefa1a6f69bdca38c083
SbattellaMattia <tiasba01@gmail.com>
2023-05-15 16:11:47 +0200

    fix carosello

--- resources/views/partials/carosello.blade.php
+++ resources/views/partials/carosello.blade.php
@@ -1,7 +1,15 @@
 <link rel="stylesheet" href="{{asset('css/partials/carosello.css')}}">
 
-<div class="slider">
-    <div class="slide-track">
+<div class="slider" style="width: calc(450px * {{$companies->count()*2}}) ;">
+    <div class="slide-track" id="width" style="width: calc(400px * {{$companies->count()*2}});">
+        @foreach($companies as $company)
+            @include('partials.card',
+[
+'image' => $company->logo,
+'color' => $company->color
+])
+        @endforeach
+
         @foreach($companies as $company)
             @include('partials.card',
 [
