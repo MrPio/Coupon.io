e673f178d1c5c5992acac1a3ad3f1a32f57a5d9c
NicoloAchilli <s1098682@studenti.univpm.it>
2023-05-16 17:28:38 +0200

    fix where page

--- resources/views/where.blade.php
+++ resources/views/where.blade.php
@@ -15,18 +15,34 @@
     <div class="padding" style="margin-top: 100px;">
         @include('partials.section_title',['title'=>'Dove siamo'])
         <div class="google_map">
-            <h3 style="margin-bottom: -10px"><strong>Contatti</strong></h3>
             <div class="container_description">
-                <ul  style=" list-style-type: disc;">
-                    <li><p><strong style="margin-right: 40px">Indirizzo:</strong>Via Brecce Bianche, 60131 Ancona AN</p></li>
-                    <li><p><strong style="margin-right: 40px">Telefono:</strong>+0734 845678</p></li>
-                    <li><p><strong style="margin-right: 57px">E-mail:</strong>coupon.io@gmail.com</p></li>
-                </ul>
+                <h2 style="padding-bottom: 20px"><strong>Contatti</strong></h2>
+                <table class="table">
+                    <tr>
+                        <th class="style_th">Indirizzo:</th>
+                        <td class="style_td">Via Brecce Bianche, 60131 Ancona AN</td>
+                    </tr>
+                    <tr>
+                        <th class="style_th">Telefono:</th>
+                        <td class="style_td">+0734 845678</td>
+                    </tr>
+                    <tr>
+                        <th class="style_th">E-mail:</th>
+                        <td class="style_td"><a href="mailto:coupon:io@gmail.com">coupon.io@gmail.com</a></td>
+                    </tr>
+                    {{--                        <ul class="list">--}}
+
+                    {{--                            --}}{{--                        <li><strong>Indirizzo: Via Brecce Bianche, 60131 Ancona AN</strong></li>--}}
+                    {{--                            --}}{{--                        <li><strong>Telefono: +0734 845678</strong></li>--}}
+                    {{--                            --}}{{--                        <li><strong>E-mail:<a href="mailto:coupon:io@gmail.com">coupon.io@gmail.com</a></strong></li>--}}
+                    {{--                        </ul>--}}
+
+                </table>
             </div>
             <iframe
                 class="map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.8963015057925!2d13.513717454350166!3d43.58787625224827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132d80235c260cdf%3A0x3ec7eb70115b435b!2sUniversit%C3%A0%20Politecnica%20delle%20Marche%20-%20Segreteria%20Studenti%20di%20Ingegneria%20-%20Agraria%20-%20Scienze!5e0!3m2!1sit!2sit!4v1684144597567!5m2!1sit!2sit"
-                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrad"></iframe>
         </div>
     </div>
 @endsection
--- resources/views/who.blade.php
+++ resources/views/who.blade.php
@@ -15,7 +15,7 @@
     <div class="padding" style="margin-top: 80px;">
         @include('partials.section_title',['title'=>'Chi siamo'])
         <div class="content">
-            <h1><strong>Coupon.io</strong></h1>
+            <h1 style="margin-bottom: 40px"><strong>Coupon.io</strong></h1>
             <p>Lorem ipsum dolor sit amen lorem ispus dolor, Lorem ipsum dolor sit amen lorem ispus dolor,Lorem ipsum
                 dolor sit amen lorem ispus dolor</p>
         </div>
