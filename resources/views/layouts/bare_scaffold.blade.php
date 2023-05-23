5c248ebf540dcc782b6f8800cfa592cc714284ca
MrPio <valeriomorelli50@gmail.com>
2023-05-23 12:20:31 +0200

    algoritmo di numerazione promozioni per aziende velocizzato di 10x (~200ms --> ~20ms)

--- resources/views/layouts/bare_scaffold.blade.php
+++ resources/views/layouts/bare_scaffold.blade.php
@@ -1,8 +1,8 @@
         <!DOCTYPE html>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <link rel="icon" href="{{ asset('images/cart.svg') }}" type="image/x-icon">
 <head>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
